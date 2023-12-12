c58a669e6ffc5237a601a6f0fc3039b7018f4790
npminhtri <136950530+npminhtri@users.noreply.github.com>
2023-12-11 17:06:24 -0700

    message

--- Project.lean
+++ Project.lean
@@ -5,8 +5,9 @@ import Mathlib.Init.Align
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
 import Mathlib.Analysis.InnerProductSpace.PiL2
-
-
+import Mathlib.Logic.Equiv.LocalEquiv
+import Mathlib.Topology.Sets.Opens
+import Mathlib.Tactic
 def hello_world := hello ++ " world"
 
 universe u
@@ -19,28 +20,60 @@ def Special_lineargroup_order_2 [Zero (Fin 4)] : Type :=
 
 
 /- Definition of four charts that covers SL(2,ℝ )-/
-def Firstchart  [Zero (Fin 4)] : Type :=
-  { x : EuclideanSpace ℝ (Fin 4) // (x 1)*(x 4) - (x 2)*(x 3) =1 ∧ (x 1) ≠ 0}
-def Secondchart  [Zero (Fin 4)] : Type :=
-  { x : EuclideanSpace ℝ (Fin 4) // (x 1)*(x 4) - (x 2)*(x 3) =1 ∧ (x 2) ≠ 0}
-def Thirdchart  [Zero (Fin 4)] : Type :=
-  { x : EuclideanSpace ℝ (Fin 4) // (x 1)*(x 4) - (x 2)*(x 3) =1 ∧ (x 3) ≠ 0}
-def Fourthchart  [Zero (Fin 4)] : Type :=
-  { x : EuclideanSpace ℝ (Fin 4) // (x 1)*(x 4) - (x 2)*(x 3) =1 ∧ (x 4) ≠ 0}
+def Firstcover  [Zero (Fin 4)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 0) ≠ 0}
+def Secondcover  [Zero (Fin 4)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 1) ≠ 0}
+def Thirdcover  [Zero (Fin 4)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2)  =1 ∧ (x 2) ≠ 0}
+def Fourthcover  [Zero (Fin 4)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 3) ≠ 0}
 /- This is use to define a topological structure on SL(2,ℝ)
 -/
 section
 
 instance [Zero (Fin 4)] : TopologicalSpace (Special_lineargroup_order_2) := instTopologicalSpaceSubtype
-instance [Zero (Fin 4)]: TopologicalSpace (Firstchart) :=
+instance [Zero (Fin 4)]: TopologicalSpace (Firstcover) :=
   instTopologicalSpaceSubtype
-instance [Zero (Fin 4)]: TopologicalSpace (Secondchart) := by apply
+instance [Zero (Fin 4)]: TopologicalSpace (Secondcover) := by apply
   instTopologicalSpaceSubtype
-instance [Zero (Fin 4)]: TopologicalSpace (Thirdchart) :=
+instance [Zero (Fin 4)]: TopologicalSpace (Thirdcover) :=
   instTopologicalSpaceSubtype
-instance [Zero (Fin 4)]: TopologicalSpace (Fourthchart) :=
+instance [Zero (Fin 4)]: TopologicalSpace (Fourthcover) :=
   instTopologicalSpaceSubtype
 
+open scoped Manifold
+
+#check {(x,y) | x ≠ 0}
+/- Construction of the chart -/ 
+variable (x y z : ℝ )
+def Euclideanwithoutplane0 : Type :=  
+def Euclideanwithoutplane1  [Zero (Fin 3)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 3) // 0 ≠  x 1  }
+def Euclideanwithoutplane2  [Zero (Fin 3)] : Type :=
+  { x : EuclideanSpace ℝ (Fin 3) // 0 ≠  x 2  }
+
+instance [Zero (Fin 2)]: TopologicalSpace (Euclideanwithoutplane0) :=
+  instTopologicalSpaceSubtype
+instance [Zero (Fin 3)]: TopologicalSpace (Euclideanwithoutplane1) :=
+  instTopologicalSpaceSubtype
+instance [Zero (Fin 3)]: TopologicalSpace (Euclideanwithoutplane2) :=
+  instTopologicalSpaceSubtype
+
+def f (x: Firstcover) : Euclideanwithoutplane0 := sorry
+def Firstchart : LocalHomeomorph (Firstcover) (Euclideanwithoutplane0) where
+  toFun x := sorry
+  invFun := sorry
+  source := sorry
+  target := sorry
+  map_source' := sorry
+  map_target' := sorry
+  left_inv' := sorry
+  right_inv' := sorry
+  open_source := sorry
+  open_target := sorry
+  continuous_toFun := sorry
+  continuous_invFun := sorry
 
 
 /-!
