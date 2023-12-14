6755579ec6b9114d3300c20a05ed6eedfb825934
npminhtri <136950530+npminhtri@users.noreply.github.com>
2023-12-13 20:27:52 -0700

    partly

--- Project.lean
+++ Project.lean
@@ -1,6 +1,7 @@
 import Project.Basic
 import Mathlib.Topology.Instances.Real
-
+import Mathlib.Logic.Equiv.PartialEquiv
+import Mathlib.Topology.Sets.Opens
 import Mathlib.Init.Align
 import Mathlib.Geometry.Manifold.LocalInvariantProperties
 import Mathlib.Geometry.Manifold.SmoothManifoldWithCorners
@@ -10,7 +11,6 @@ import Mathlib.Tactic
 import Mathlib.Data.Set.Basic
 import Mathlib.Data.Real.Basic
 
-#check { (x, y, z) : ℝ × ℝ × ℝ | x + y + z ≠ 0}
 def hello_world := hello ++ " world"
 
 universe u
@@ -18,13 +18,13 @@ open TopologicalSpace
 noncomputable section
 
 /- Definition of SL(2,ℝ ) and charts by hand -/
-def Special_lineargroup_order_2 : Type :=
-   { x : EuclideanSpace ℝ (Fin 4) // (x 1)*(x 4) - (x 2)*(x 3) =1 }
+def SL2R : Type :=
+   { (x, y, z,t) : ℝ × ℝ × ℝ × ℝ | x*z-y*t =1 }
 
 
 /- Definition of four charts that covers SL(2,ℝ )-/
 def Firstcover : Type :=
-  { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 0) ≠ 0}
+ { (x, y, z,t) : ℝ × ℝ × ℝ × ℝ | x*t-y*z =1 ∧ x ≠ 0 }
 def Secondcover  : Type :=
   { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 1) ≠ 0}
 def Thirdcover   : Type :=
@@ -33,11 +33,11 @@ def Fourthcover : Type :=
   { x : EuclideanSpace ℝ (Fin 4) // (x 0)*(x 3) - (x 1)*(x 2) =1 ∧ (x 3) ≠ 0}
 /- This is use to define a topological structure on SL(2,ℝ)
 -/
+
 section
 
-instance : TopologicalSpace (Special_lineargroup_order_2) := instTopologicalSpaceSubtype
-instance : TopologicalSpace (Firstcover) :=
-  instTopologicalSpaceSubtype
+instance : TopologicalSpace (SL2R) := instTopologicalSpaceSubtype
+instance : TopologicalSpace (Firstcover) := instTopologicalSpaceSubtype
 instance : TopologicalSpace (Secondcover) := by apply
   instTopologicalSpaceSubtype
 instance : TopologicalSpace (Thirdcover) :=
@@ -50,37 +50,64 @@ open scoped Manifold
 
 /- Construction of the chart -/
 def Euclideanwithoutplane0 : Type :=
-  { x : EuclideanSpace ℝ (Fin 3) // 0 ≠  x 0  }
+ { (x, y, z) : ℝ × ℝ × ℝ  | x ≠ 0 }
 
 def Euclideanwithoutplane1 : Type :=
   { x : EuclideanSpace ℝ (Fin 3) // 0 ≠  x 1  }
 def Euclideanwithoutplane2  [Zero (Fin 3)] : Type :=
   { x : EuclideanSpace ℝ (Fin 3) // 0 ≠  x 2  }
 
+def Projection43 (x: EuclideanSpace ℝ (Fin 4) ) :
 
 instance : TopologicalSpace (Euclideanwithoutplane0) :=
   instTopologicalSpaceSubtype
 
-instance [Zero (Fin 3)]: TopologicalSpace (Euclideanwithoutplane1) :=
+instance : TopologicalSpace (Euclideanwithoutplane1) :=
   instTopologicalSpaceSubtype
-instance [Zero (Fin 3)]: TopologicalSpace (Euclideanwithoutplane2) :=
+instance : TopologicalSpace (Euclideanwithoutplane2) :=
   instTopologicalSpaceSubtype
 
+variable (a: Firstcover)
+#check a.2.2
+
+@[simp]
+def prodfst (a: ℝ × ℝ × ℝ × ℝ  ) :ℝ × ℝ × ℝ :=  (a.1, a.2.1,a.2.2.1)
+@[simp]
+def inversefst (a: ℝ × ℝ × ℝ  ) :ℝ × ℝ × ℝ × ℝ := (a.1, a.2.1, a.2.2, (1+a.2.1*a.2.2)/(a.1))
+
+/- If a vector ∈ ℝ⁴ has nonzero first coordinate, then so is it projection on the space spanned by the first three coordinate -/
+@[simp]
+theorem nonzerofst (a: ℝ × ℝ × ℝ × ℝ )(ha: a.1 ≠ 0) : prodfst a ∈ {(x,y,z): ℝ × ℝ × ℝ | x ≠0 } := by
+  intro B; exact ha B
+
+@[simp]
+theorem nonzerofst1 (a: ℝ × ℝ × ℝ   )(ha: a.1 ≠ 0) :
+inversefst a ∈  { (x, y, z,t) : ℝ × ℝ × ℝ × ℝ | x*t-y*z=1 ∧ x ≠ 0 }:= by
+  simp;
+  apply And.intro
+  .field_simp; ring
+  .exact ha
 
 
-def Firstchart : LocalHomeomorph (Firstcover) (Euclideanwithoutplane0) where
-  toFun x :={ val :=  , property:= sorry}
-  invFun := sorry
-  source := sorry
-  target := sorry
-  map_source' := sorry
-  map_target' := sorry
-  left_inv' := sorry
-  right_inv' := sorry
-  open_source := sorry
-  open_target := sorry
-  continuous_toFun := sorry
-  continuous_invFun := sorry
+
+def phi1 : PartialHomeomorph (Firstcover) (Euclideanwithoutplane0) where
+  toFun a := by
+    constructor
+    . apply nonzerofst; exact a.2.2
+  invFun a := by
+    constructor
+    apply nonzerofst1; exact a.2
+  source := by exact Set.univ
+  target := by exact Set.univ
+  map_source' := by simp
+  map_target' := by simp
+  left_inv' := by simp; intro x; ring_nf; sorry
+  right_inv' := by simp
+  open_source := by simp
+  open_target := by simp
+  continuousOn_toFun := by simp; sorry
+  continuousOn_invFun := by simp; sorry
+
 
 
 /-!
